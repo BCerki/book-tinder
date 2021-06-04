9ffc8a3c827d33ded181b0f7f9582e31809c7a1d
unknown <briannac@shaw.ca>
2021-06-04 23:23:07 +0100

    set up boilerplate with chatbot

---
+++ react-front-end/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SimpleForm from "./components/chatbot/SimpleForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SimpleForm />
+      </div>
+    );
+  }
+}
+
+export default App;
