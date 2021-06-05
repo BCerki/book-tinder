abf1d166a4ae1ec1415d18b7c7ec8ca00798baca
unknown <briannac@shaw.ca>
2021-06-05 16:48:44 +0100

    create dummy data files (to get real data from backend api)

---
+++ react-front-end/src/dummyBookData.js
@@ -0,0 +1,19 @@
+//probably can't actually have message data in here
+const bookData = [
+  {
+    id: 1,
+    title: "Flowers for Algeron",
+    coverImage:
+      "https://books.google.com/books/content?id=64tuPwAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+    latestMessage: "Heeeeeeey",
+  },
+  {
+    id: 2,
+    title: "Another book",
+    coverImage:
+      "https://books.google.com/books/content?id=34tuPwAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+    latestMessage: "Hi",
+  },
+];
+
+module.exports = bookData;
