5a826afb728c88059e39569bd4e195463e391d79
unknown <briannac@shaw.ca>
2021-06-05 18:46:34 +0100

    add lodash to swipe view for choosing a random book

--- react-front-end/src/components/SwipeView.js
+++ react-front-end/src/components/SwipeView.js
@@ -1,5 +1,8 @@
 import React from "react";
+import bookData from "../dummyData/dummyBookData";
+import _ from "lodash";
 
 export default function Swipe(props) {
-  return <img />;
+  const randomBook = _.shuffle(bookData)[0];
+  return <img src={randomBook.coverImage} alt={randomBook.title} />;
 }
