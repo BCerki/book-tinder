c04a8da22a70c967afcc2fc1472301db2ca8ab73
unknown <briannac@shaw.ca>
2021-06-05 17:19:14 +0100

    add all book dummy data

--- react-front-end/src/dummyData/dummyBookData.js
+++ react-front-end/src/dummyData/dummyBookData.js
@@ -3,15 +3,27 @@ const bookData = [
   {
     id: 1,
     title: "Flowers for Algeron",
+    author: "Author",
     coverImage:
       "https://books.google.com/books/content?id=64tuPwAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+    description: "describe",
+    isbn: "1234567891234",
+    pageCount: 5,
+    age: 5,
+    price: 6,
     latestMessage: "Heeeeeeey",
   },
   {
     id: 2,
     title: "Another book",
+    author: "author",
     coverImage:
       "https://books.google.com/books/content?id=34tuPwAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+    description: "describe",
+    isbn: "1234567891234",
+    pageCount: 5,
+    age: 5,
+    price: 6,
     latestMessage: "Hi",
   },
 ];
