8624cc8469e653f3821f5ae4a48aeafaa45e7c12
unknown <briannac@shaw.ca>
2021-06-06 22:37:59 +0100

    position nav bar and set up scrolling on main

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -13,21 +13,22 @@ export default function App(props) {
   const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
-    console.log("handleChange fired");
     setValue(newValue);
   };
   console.log("value is", value);
   return (
-    <main>
-      <section className="view">
-        {value === 0 && <SwipeView />}
-        {value === 1 && <MiddleView />}
-        {value === 2 && <ProfileView />}
-      </section>
+    <>
+      <main>
+        <section className="view">
+          {value === 0 && <SwipeView />}
+          {value === 1 && <MiddleView />}
+          {value === 2 && <ProfileView />}
+        </section>
+      </main>
       <footer>
         <NavBar onChange={handleChange} value={value} />
       </footer>
-    </main>
+    </>
   );
 }
 
