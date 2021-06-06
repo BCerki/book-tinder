594a7642613f6fb21331c9e009a959cda771131b
unknown <briannac@shaw.ca>
2021-06-06 21:29:48 +0100

    debug book axios call

--- react-front-end/src/components/SwipeView.js
+++ react-front-end/src/components/SwipeView.js
@@ -1,35 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import bookData from "../dummyData/dummyBookData";
 import _ from "lodash";
 import "../styles/swipe.scss";
 
 export default function Swipe(props) {
-  //current book is an object of book details
-  const [booksState, setBooksState] = useState({
-    //OK to have a dummy?
-    currentBook: null,
-    allBooks: [],
-  });
+  const [currentBook, setCurrentBook] = useState({});
   //bad to requery the api on every mount/render?
-  axios
-    .get("/api/books")
-    .then((response) => {
-      setBooksState((prev) => ({
-        ...prev,
-        currentBook: _.shuffle(response)[0],
-        books: response,
-      }));
-
-      return (
-        <div>
-          <img
-            className="cover"
-            src={currentBook.coverImage}
-            alt={currentBook.title}
-          />
-        </div>
-      );
-    })
-    .catch((err) => console.log(err));
+  // _.shuffle(response.data)[0]
+  useEffect(() => {
+    axios
+      .get("https://my-json-server.typicode.com/bcerki/book-tinder/books")
+      .then((response) => {
+        setCurrentBook(response.data);
+      });
+  }, []);
+  return (
+    <img
+      className="cover"
+      src={currentBook.coverImage}
+      alt={currentBook.title}
+    />
+  );
 }
+
+// {
+/*  */
+// }
