01543eee78affb13442c0c5dd2ace1076682f38a
unknown <briannac@shaw.ca>
2021-06-06 03:10:59 +0100

    set up styles for swipe view

--- react-front-end/src/components/SwipeView.js
+++ react-front-end/src/components/SwipeView.js
@@ -1,9 +1,18 @@
 import React from "react";
 import bookData from "../dummyData/dummyBookData";
 import _ from "lodash";
+import "../styles/swipe.scss";
 
 export default function Swipe(props) {
   const randomBook = _.shuffle(bookData)[0];
   //set state here
-  return <img src={randomBook.coverImage} alt={randomBook.title} />;
+  return (
+    <div>
+      <img
+        className="cover"
+        src={randomBook.coverImage}
+        alt={randomBook.title}
+      />
+    </div>
+  );
 }
--- react-front-end/src/dummyData/dummyBookData.js
+++ react-front-end/src/dummyData/dummyBookData.js
@@ -18,7 +18,7 @@ const bookData = [
     title: "Another book",
     author: "author",
     coverImage:
-      "https://books.google.com/books/content?id=34tuPwAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+      "http://books.google.com/books/content?id=ztmODQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
     description: "describe",
     isbn: "1234567891234",
     pageCount: 5,
