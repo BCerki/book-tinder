41a96dd228bf7f0ff66dda6f10963742251ccab6
unknown <briannac@shaw.ca>
2021-06-06 16:02:39 +0100

    refactor App into functional component

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -7,67 +7,37 @@ import NavBar from "./Components/NavBar";
 import MiddleView from "./Components/MiddleView";
 import ChatView from "./Components/ChatView";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "Click the button to load data!",
-    };
-  }
-
-  fetchData = () => {
-    axios
-      .get("/api/data") // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        console.log(response.data); // The entire response from the Rails API
-
-        console.log(response.data.message); // Just the message
-        this.setState({
-          message: response.data.message,
-        });
-      });
-  };
-
-  render() {
-    return (
-      <main>
-        <div className="App">
-          <h1>{this.state.message}</h1>
-          <button onClick={this.fetchData}>Fetch Data</button>
-        </div>
-        <section>
-          This is the nav component:
-          <NavBar />
-        </section>
-        <section>
-          This is the profile view:
-          <ProfileView />
-        </section>
-
-        <section>
-          this is the middle view:
-          <MiddleView />
-        </section>
-        <section>
-          this is the ChatBot view:
-          <ChatView />
-        </section>
-        <section>
-          this is the Swipe view:
-          <br />
-          <SwipeView />
-        </section>
-      </main>
-    );
-  }
-
-  // render() {
-  //   return (
-  //     <div>
-  //       <SimpleForm />
-  //     </div>
-  //   );
-  // }
+export default function App() {
+  return (
+    <main>
+      <section></section>
+      <footer>
+        This is the nav component:
+        <NavBar />
+      </footer>
+      <section>
+        this is the ChatBot view:
+        <ChatView />
+      </section>
+    </main>
+  );
 }
-export default App;
+
+// fetchData = () => {
+//   axios
+//     .get("/api/data") // You can simply make your requests to "/api/whatever you want"
+//     .then((response) => {
+//       // handle success
+//       console.log(response.data); // The entire response from the Rails API
+
+//       console.log(response.data.message); // Just the message
+//       this.setState({
+//         message: response.data.message,
+//       });
+//     });
+// };
+
+// <div className="App">
+//           <h1>{this.state.message}</h1>
+//           <button onClick={this.fetchData}>Fetch Data</button>
+//         </div>
