9805ccdf24eb6c5c5e79863e163e7f41ac493d64
A <awongpro7@gmail.com>
2021-06-07 18:30:35 +0000

    Pre-merge

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
-import SimpleForm from "./components/chatbot/SimpleForm";
 
 class App extends Component {
   constructor(props) {
