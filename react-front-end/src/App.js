abc536e6ea801816b56939681f9f339619c156af
unknown <briannac@shaw.ca>
2021-06-08 17:57:21 +0100

    refact App.js to use react router instead of conditional views

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,128 +1,36 @@
-import React, { useState, Component } from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Switch, Route } from "react-dom";
-import Footer from "./Components/Footer";
-// import "./App.css";
-// import SimpleForm from "./components/chatbot/SimpleForm";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-/* Adrian's working axios/render code
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [],
-      message: "Click the button to load data right now!",
-    };
-  }
-
-  fetchData = () => {
-    axios
-      .get("/api/test") // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // console.log("LOG: response:", response);
-        this.setState({
-          books: response.data,
-        });
-      });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h1>{this.state.message}</h1>
-        <div>{JSON.stringify(this.state.books)}</div>
-        <button onClick={this.fetchData}>Fetch Data</button>
-      </div>
-    );
-  }
-
-  Chatbot code
-  render() {
-    return (
-      <div>
-        <SimpleForm />
-      </div>
-    );
-  }
-*/
-
-import "./App.scss";
+//Import components
 import ProfileView from "./Components/ProfileView";
 import SwipeView from "./Components/SwipeView";
-import NavBar from "./Components/NavBar";
-import BooksView from "./Components/BooksView";
 import ChatView from "./Components/ChatView";
+import BooksView from "./Components/BooksView";
+import Footer from "./Components/Footer";
 
-import { values } from "./dummyData/dummyBookData";
-import classNames from "classnames";
+//Import styling
+import "./App.scss";
 
-export default function App(props) {
-  // State and handlers for the nav bar
-  const [value, setValue] = useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  //Styling
-  let mainClass = classNames(
-    { swipe: value === 0 },
-    { middle: value === 1 },
-    { profile: value === 2 }
-  );
+export default function AppRouter() {
   return (
-    <>
-      <main className={mainClass}>
-        {value === 0 && <SwipeView />}
-        {value === 1 && <BooksView />}
-        {value === 2 && <ProfileView />}
+    <Router>
+      <main>
+        <Switch>
+          <Route path="/middle/:book">
+            <ChatView />
+          </Route>
+          <Route path="/middle">
+            <BooksView />
+          </Route>
+          <Route path="/profile">
+            <ProfileView />
+          </Route>
+          <Route path="/">
+            <SwipeView />
+          </Route>
+        </Switch>
       </main>
-      <footer>
-        <NavBar onChange={handleChange} value={value} />
-      </footer>
-    </>
+      <Footer />
+    </Router>
   );
-
-  //   return (
-  //     <Router>
-  //       <Switch>
-  //         <Route path="/chat/:book">
-  //           <Header backButton="chat" />
-  //           <ChatView />
-  //         </Route>
-
-  //         <Route path="/chat">
-  //           <Header backButton="/" />
-  //           <ChatView />
-  //         </Route>
-
-  //         <Route path="/">
-  //           <Header />
-  //           <MiddleView />
-  //         </Route>
-
-  //         <Route path="/profile">
-  //           <Header />
-  //           <ProfileView />
-  //         </Route>
-  //       </Switch>
-  //     </Router>
-  //   );
 }
-
-// fetchData = () => {
-//   axios
-//     .get("/api/data") // You can simply make your requests to "/api/whatever you want"
-//     .then((response) => {
-//       // handle success
-//       console.log(response.data); // The entire response from the Rails API
-
-//       console.log(response.data.message); // Just the message
-//       this.setState({
-//         message: response.data.message,
-//       });
-//     });
-// };
-
-// <div className="App">
-//           <h1>{this.state.message}</h1>
-//           <button onClick={this.fetchData}>Fetch Data</button>
-//         </div>
