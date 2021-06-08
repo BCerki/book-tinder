4d7afc4dfdf85e81de886f4fbcba04c745b7e154
unknown <briannac@shaw.ca>
2021-06-08 17:22:45 +0100

    adjust spacing height of <main>

--- react-front-end/src/Components/Footer.js
+++ react-front-end/src/Components/Footer.js
@@ -4,7 +4,7 @@ import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import MenuBookIcon from "@material-ui/icons/MenuBook";
-import ChatIcon from "@material-ui/icons/Chat";
+import ForumIcon from "@material-ui/icons/Forum";
 import AccountBoxIcon from "@material-ui/icons/AccountBox";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -21,7 +21,7 @@ export default function Footer(props) {
       </div>
       <div>
         <Link to="/middle">
-          <ChatIcon />
+          <ForumIcon />
         </Link>
       </div>
       <div>
