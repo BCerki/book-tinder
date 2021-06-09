ce39dac5cdfa5f17c720f02be450799a3f37fd1b
unknown <briannac@shaw.ca>
2021-06-09 16:19:36 +0100

    add BookStateProvider to books view and back bar

--- react-front-end/src/Components/BackBar.js
+++ react-front-end/src/Components/BackBar.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { bookStateContext } from "../providers/BookStateProvider";
+
+//import components
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
@@ -8,19 +12,30 @@ import ChatIcon from "@material-ui/icons/Chat";
 import Avatar from "@material-ui/icons/AccountBox";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import BlockIcon from "@material-ui/icons/Block";
 
 //Styling
 import "../styles/backbar.scss";
+import axios from "axios";
 
 export default function BackBar(props) {
+  const { currentBook, block } = useContext(bookStateContext);
+  console.log("currentBook.image in backbar", currentBook);
+
   return (
     <div className="backBar">
       <Link to="/books">
         <ArrowBackIosIcon />
       </Link>
-      <Avatar className="chatAvatar" />
+      <Avatar className="chatAvatar" src={currentBook.image} />
       <div>
-        <ArrowBackIosIcon className="hidden" />
+        <Link to="/books">
+          <BlockIcon
+            onClick={() => {
+              block(currentBook.id);
+            }}
+          />
+        </Link>
       </div>
     </div>
   );
