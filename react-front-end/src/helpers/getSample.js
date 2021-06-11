7d5d46b0c2842e19e98abd56d4034d34808c00ec
unknown <briannac@shaw.ca>
2021-06-11 17:23:31 +0100

    add helper functions folder to front-end

---
+++ react-front-end/src/helpers/getSample.js
@@ -0,0 +1,48 @@
+const crawler = require("crawler-request");
+const _ = require("lodash");
+
+// Calls BiblioShare API, requests PDF sample, peforms conversions, returns single random sentence
+const getSample = function (isbn) {
+  return new Promise((resolve, reject) => {
+    // Calls API, converts PDF to rough text
+    crawler(
+      `https://www.biblioshare.ca/BNCServices/BNCServices.asmx/Samples?token=3e58phfbom2hx2z7&ean=${isbn}&san=&perspective=Sample&filenumber=`
+    )
+      // Handle response
+      .then(function (response) {
+        // Store result of rough PDF to text conversion
+        const convertedText = response.text;
+        // Encode convertedText into URI
+        const encondedText = encodeURI(convertedText);
+        // Split encondedText by new line (%0A), each element represents line of text, store as array
+        const splitArr = encondedText.split("%0A");
+        // Decode array line by line to make output readable
+        const decodedArr = splitArr.map((line) => decodeURI(line));
+        // Filter splitArr removing lines that start with numbers & headings, have empty content
+        const filteredArr = decodedArr.filter((line) => {
+          return (
+            !line.match("^\\d") && !line.match("^[A-Z]{2}") && line.length > 0
+          );
+        });
+        // Remove double spaces
+        const joinedStr = filteredArr.join("").replace(/\s\s+/g, " ");
+        // Split cleaned up whole text (no line breaks) by ".", store as array
+        const allLinesArr = joinedStr.split(".");
+        // Shuffle array
+        const randomLine = _.shuffle(allLinesArr)[0];
+        // Console log result
+        console.log(`Random line from ISBN ${isbn}: ${randomLine}`);
+        // Return single random line
+        resolve(randomLine);
+      })
+      // Error handling
+      .catch((err) => {
+        reject(err.message);
+      });
+  });
+};
+
+// Test code
+// getSample(9781770415034);
+
+module.exports = getSample;
---
+++ react-front-end/src/helpers/googleAPIcall.js
@@ -0,0 +1,56 @@
+const { GOOGLE_BOOK_KEY } = require("../.secrets");
+const axios = require("axios");
+
+//to search only subject, put "insubject:" in front of the query term, for example: "insubject:fiction"
+const query = "mystery";
+
+const helper = function (arg) {
+  let results = null;
+  arg.find((element) => {
+    if (element.identifier.length === 13) {
+      results = element.identifier;
+    }
+    return null;
+  });
+
+  return results;
+};
+axios
+  .get(
+    `https://www.googleapis.com/books/v1/volumes?q=insubject:${query}&key=${GOOGLE_BOOK_KEY}`
+  )
+  .then((response) => {
+    for (let i = 0; i < 10; i++) {
+      console.log(
+        `VALUES ('${helper(
+          response.data.items[i].volumeInfo.industryIdentifiers
+        )}','${response.data.items[i].volumeInfo.title}', '${
+          response.data.items[i].volumeInfo.authors[0]
+        }', '${response.data.items[i].volumeInfo.imageLinks.thumbnail}', '${
+          response.data.items[i].searchInfo.textSnippet
+        }', ${response.data.items[i].volumeInfo.pageCount}, '${
+          response.data.items[i].volumeInfo.publishedDate
+        }', ${
+          response.data.items[i].saleInfo.listPrice
+            ? response.data.items[i].saleInfo.listPrice.amount
+            : null
+        }, false)`
+      );
+    }
+  });
+//authors is array
+
+// const getISBN13 = function() {
+//   const thing = response.data.items[
+//     i
+//   ].volumeInfo.industryIdentifiersindustryIdentifiers.find(
+//     (element) => element.identfier.length === 13
+//   );
+//   console.log("thing", thing);
+// };
+
+// response.data.items[i].volumeInfo.industryIdentifiersindustryIdentifiers.find(
+//   (element) => {
+//     if (element.identfier.length === 13) return element.identfier;
+//   }
+// );
