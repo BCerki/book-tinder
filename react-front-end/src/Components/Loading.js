bb8f91b573795746751d2540405a095370d41b2d
unknown <briannac@shaw.ca>
2021-06-11 23:00:08 +0100

    style loading animations

--- react-front-end/src/Components/Loading.js
+++ react-front-end/src/Components/Loading.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { DiamonLoading } from "react-loadingg";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    "& > * + *": {
-      marginLeft: theme.spacing(2),
-    },
-  },
-}));
-
 export default function Loading() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <CircularProgress />
+    <div className={"spinner"}>
+      <DiamonLoading />
     </div>
   );
 }
