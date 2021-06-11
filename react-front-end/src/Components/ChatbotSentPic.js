8e76c112d41ac5f3275111f0e6b42c81649cbcf3
unknown <briannac@shaw.ca>
2021-06-11 05:17:20 +0100

    add console.logs and debugging text to chatbotsentpic component

--- react-front-end/src/Components/ChatbotSentPic.js
+++ react-front-end/src/Components/ChatbotSentPic.js
@@ -3,9 +3,12 @@ import { chatBookStateContext } from "../providers/ChatBookStateProvider";
 
 export default function ChatbotSentPic(props) {
   const { currentChatBook, chatContext } = useContext(chatBookStateContext);
+
+  console.log("currentChatBook in chatbotsentpic is:", currentChatBook);
   return (
     <span>
-      <img src={currentChatBook.img} alt={currentChatBook.title} />
+      currentChatBook img is:
+      <img src={currentChatBook.image} alt={currentChatBook.title} />
     </span>
   );
 }
