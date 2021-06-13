2e42853766d34a38dd9e9b0c483ca1fe1a473d58
unknown <briannac@shaw.ca>
2021-06-13 17:51:05 +0100

    add the correct parameters to BookManager location call in BookManagerLocation component

--- react-front-end/src/Components/BookManagerLocation.js
+++ react-front-end/src/Components/BookManagerLocation.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-//<div className="isbn">ISBN: {props.isbn}</div>
+
 import axios from "axios";
 export default function BookManagerLocation(props) {
   const [date, setDate] = useState("");
@@ -10,6 +10,9 @@ export default function BookManagerLocation(props) {
     useLocation().pathname.replace("/matches/", "")
   );
 
+  const userId = props.userId;
+  console.log("userId is", userId);
+
   useEffect(() => {
     axios.get(`/api/users/:id/conversations`).then((result) => {
       //set conversation state
@@ -18,17 +21,22 @@ export default function BookManagerLocation(props) {
         (conversation) => conversation.id === conversationId
       );
       setCurrentConversation(thisConversation);
+      console.log("the user id is  is", thisConversation.user_id);
 
-      axios
-        .get(
-          `api/getlocation?isbn=${thisConversation.isbn}postal=${props.postalCode}&max_distance=${props.maxDistance}`
-        )
-        .then((result) => {
-          setDate(result);
-        })
-        .catch();
+      axios.get(`/api/users/${thisConversation.user_id}`).then((result) => {
+        console.log("user info is", result.data[0]);
+        const postalCode = result.data[0].postalCode;
+        const maxDistance = result.data[0].maxDistance;
+        axios
+          .get(
+            `api/getlocation?isbn=${thisConversation.isbn}postal=${postalCode}&max_distance=${maxDistance}`
+          )
+          .then((result) => {
+            setDate(result);
+          });
+      });
     });
-  });
+  }, []);
 
   return <div>Here is the BookManager response: {date}</div>;
 }
