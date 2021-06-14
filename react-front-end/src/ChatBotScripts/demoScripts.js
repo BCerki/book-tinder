8ca30dcaec6d81e2d31598bcf849ab60be7ff4ba
unknown <briannac@shaw.ca>
2021-06-14 03:53:32 +0100

    create placeholder demo scripts

---
+++ react-front-end/src/ChatBotScripts/demoScripts.js
@@ -0,0 +1,7 @@
+const outOfTheAttic = [];
+const theForestCityKiller = [];
+const allTheLeavings = [];
+
+const demoScripts = { outOfTheAttic, theForestCityKiller, allTheLeavings };
+
+export default demoScripts;
