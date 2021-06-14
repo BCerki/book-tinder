f5277310c03e3bcb5f90245cdc0d1082f2462ec9
unknown <briannac@shaw.ca>
2021-06-14 22:05:28 +0100

    add credits to tagline component

--- react-front-end/src/Components/Tagline.js
+++ react-front-end/src/Components/Tagline.js
@@ -7,7 +7,27 @@ export default function Tagline(props) {
       <div className={"tagline"}>
         <div className={"credits"}>
           Data provided by{" "}
-          <a href="https://www.booknetcanada.ca/biblioshare">BNC BiblioShare</a>
+          <a
+            className={"creditsLink"}
+            href="https://www.booknetcanada.ca/biblioshare"
+          >
+            BNC BiblioShare
+          </a>
+          , BookManager, and Google Books. Photos by{" "}
+          <a
+            className={"creditsLink"}
+            href="https://unsplash.com/@dollargill?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          >
+            Dollar Gill
+          </a>{" "}
+          (this page) and{" "}
+          <a
+            className={"creditsLink"}
+            href="https://unsplash.com/@laurachouette?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          >
+            Laura Chouette
+          </a>{" "}
+          (match page).
         </div>
       </div>
     </Link>
