34807a0fef232f3328b92f48bafdc49d39dfced6
unknown <briannac@shaw.ca>
2021-06-15 05:01:55 +0100

    seed 8 matches and their conversation history

---
+++ express-back-end/db/seeds/04_seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO conversations (user_id,book_id, message)
+VALUES(1,1,'[{"currentStep":"1","previousStep":"2","previousSteps":"3","renderedSteps":"4"},{"id":"5","message":"6"},{},["7"],["7"],"1","Heeeeeeeeeeey, bookworm 😘",{"delay":1000,"avatar":"8","id":"5","message":"6"},"from state"]'),
+(1,2,'[{"currentStep":"1","previousStep":"2","previousSteps":"3","renderedSteps":"4"},{"id":"5","message":"6"},{},["7"],["7"],"1","Maybe we could end the night between my endsheets? 📖💓📖",{"delay":1000,"avatar":"8","id":"5","message":"6"},"from state"]'),
+(1,3,'[{"currentStep":"1","previousStep":"2","previousSteps":"3","renderedSteps":"4"},{"id":"5","message":"6"},{},["7"],["7"],"1","Hey bibliophile, interested in starting a new chapter together?",{"delay":1000,"avatar":"8","id":"5","message":"6"},"from state"]'),
+(1,4,'[{"currentStep":"1","previousStep":"2","previousSteps":"3","renderedSteps":"4"},{"id":"5","message":"6"},{},["7"],["7"],"1","How would you feel about romancing the tome with me?",{"delay":1000,"avatar":"8","id":"5","message":"6"},"from state"]'),
+(1,5,'[{"currentStep":"1","previousStep":"2","previousSteps":"3","renderedSteps":"4"},{"id":"5","message":"6"},{},["7"],["7"],"1","Well hello bibliophile, want to join me between my covers? 📕💞📕",{"delay":1000,"avatar":"8","id":"5","message":"6"},"from state"]'),
+(1,6,'[{"currentStep":"1","previousStep":"2","previousSteps":"3","renderedSteps":"4"},{"id":"5","message":"6"},{},["7"],["7"],"1","I bet you say that to all the books",{"delay":1000,"avatar":"8","id":"5","message":"6"},"from state"]'),
+(1,7,'[{"currentStep":"1","previousStep":"2","previousSteps":"3","renderedSteps":"4"},{"id":"5","message":"6"},{},["7"],["7"],"1","Tired of borrowing books at the library? Want to check out something fresh and novel?",{"delay":1000,"avatar":"8","id":"5","message":"6"},"from state"]'),
+(1,8,'[{"currentStep":"1","previousStep":"2","previousSteps":"3","renderedSteps":"4"},{"id":"5","message":"6"},{},["7"],["7"],"1","No, YOU''RE spineless",{"delay":1000,"avatar":"8","id":"5","message":"6"},"from state"]')
+
