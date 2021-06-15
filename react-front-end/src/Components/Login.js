2a1f5b9a9bdcadca36f797424aaf0cee3917af75
unknown <briannac@shaw.ca>
2021-06-15 18:39:37 +0100

    use material ui text fields on login page

--- react-front-end/src/Components/Login.js
+++ react-front-end/src/Components/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useContext } from "react";
 import bookupLogo from "./bookupLogo.png";
+import TextField from "@material-ui/core/TextField";
 
 import {
   BrowserRouter as Router,
@@ -39,31 +40,31 @@ export default function UserLogin() {
           <div className={"appName"}>bookUp</div>
           <img className={"logo"} src={bookupLogo} alt="bookUp logo" />
           <div className={"formFields"}>
-            <form onSubmit={onSubmit}>
-              <div>
-                <input
-                  type="text"
-                  name="login"
-                  value={email}
-                  placeholder="Username"
-                  // onChange={onEmailChange}
-                />
-              </div>
-              <div>
-                <input
-                  type="password"
-                  name="password"
-                  value={password}
-                  placeholder="Password"
-                  // onChange={onPasswordChange}
-                />
-              </div>
-              <div className="submit">
-                <Link to="/credits">
-                  <input type="submit" name="commit" value="Login" />
-                </Link>
-              </div>
-            </form>
+            {/* <form onSubmit={onSubmit}> */}
+            <div className="input">
+              <TextField
+                label="Email"
+                id="outlined-margin-none"
+                defaultValue={"djuanseeksliason@aol.com"}
+                variant="outlined"
+                fullWidth={true}
+              />
+            </div>
+            <div className="input">
+              <TextField
+                label="Password"
+                id="outlined-margin-none"
+                defaultValue={"•••••"}
+                variant="outlined"
+                fullWidth={true}
+              />
+            </div>
+            <div className="submit">
+              <Link to="/credits">
+                <input type="submit" name="commit" value="Login" />
+              </Link>
+            </div>
+            {/* </form> */}
           </div>
         </div>
       </div>
