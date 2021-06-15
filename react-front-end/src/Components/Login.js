514f9988e001873bd603ca90dffc5d350decf72c
unknown <briannac@shaw.ca>
2021-06-15 17:51:46 +0100

    experiment with fading in

--- react-front-end/src/Components/Login.js
+++ react-front-end/src/Components/Login.js
@@ -2,8 +2,15 @@ import React from "react";
 import { useState, useContext } from "react";
 import bookupLogo from "./bookupLogo.png";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import Tagline from "./Tagline";
 
 //Styling
 import "../styles/login.scss";
@@ -11,52 +18,56 @@ import "../styles/login.scss";
 export default function UserLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [credits, showCredits] = useState(false);
 
-  const onEmailChange = function(event) {
-    setEmail(event.target.value);
-  };
+  // const onEmailChange = function(event) {
+  //   setEmail(event.target.value);
+  // };
 
-  const onPasswordChange = function(event) {
-    setPassword(event.target.value);
-  };
+  // const onPasswordChange = function(event) {
+  //   setPassword(event.target.value);
+  // };
 
   const onSubmit = function(event) {
     event.preventDefault();
+    showCredits(true);
   };
-
-  return (
-    <div className={"page"}>
-      <div className={"login"}>
-        <div className={"appName"}>bookUp</div>
-        <img className={"logo"} src={bookupLogo} alt="bookUp logo" />
-        <div className={"formFields"}>
-          <form onSubmit={onSubmit}>
-            <div>
-              <input
-                type="text"
-                name="login"
-                value={email}
-                placeholder="Username"
-                onChange={onEmailChange}
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                name="password"
-                value={password}
-                placeholder="Password"
-                onChange={onPasswordChange}
-              />
-            </div>
-            <div className="submit">
-              <Link to="/credits">
-                <input type="submit" name="commit" value="Login" />
-              </Link>
-            </div>
-          </form>
+  if (!credits) {
+    return (
+      <div className={"page"}>
+        <div className={"login"}>
+          <div className={"appName"}>bookUp</div>
+          <img className={"logo"} src={bookupLogo} alt="bookUp logo" />
+          <div className={"formFields"}>
+            <form onSubmit={onSubmit}>
+              <div>
+                <input
+                  type="text"
+                  name="login"
+                  value={email}
+                  placeholder="Username"
+                  // onChange={onEmailChange}
+                />
+              </div>
+              <div>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  placeholder="Password"
+                  // onChange={onPasswordChange}
+                />
+              </div>
+              <div className="submit">
+                <Link to="/credits">
+                  <input type="submit" name="commit" value="Login" />
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <Tagline />;
 }
