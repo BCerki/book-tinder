4161a1f2203d09759f64a0dc891015c860f59ffc
Michelle Verge <michellelverge@gmail.com>
2021-06-16 03:35:59 +0000

    updates user and genres seeds

--- express-back-end/db/seeds/02_seeds.sql
+++ express-back-end/db/seeds/02_seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (name, age, page_count, price, max_distance, maturity, genres, postal_code)
-VALUES ('Sandra Gardiner', ARRAY[20, 40], ARRAY[256, 512], ARRAY[10, 30], 80, false, ARRAY['Mystery', 'Romance', 'Young Adult', 'Fiction', 'Science Fiction'], 'V6B5A1');
+VALUES ('Don Juan', ARRAY[20, 40], ARRAY[256, 512], ARRAY[10, 30], 80, false, ARRAY['Mystery', 'Romance', 'Literary', 'Science Fiction', 'Fantasy', 'Children''s', 'Non-fiction', 'History', 'Biography/Memoir', 'Cooking', 'Humour', 'Self-help'], 'V6B5A1');
 
 
 -- INSERT INTO block_user (users_id, books_id)
@@ -8,12 +8,23 @@ VALUES ('Sandra Gardiner', ARRAY[20, 40], ARRAY[256, 512], ARRAY[10, 30], 80, fa
 INSERT INTO genres (name)
 VALUES('Mystery'),
 ('Romance'),
+('Literary'),
 ('Young Adult'),
-('Biography'),
+('Biography/Memoir'),
 ('Historical Fiction'),
 ('Classics'),
 ('Fiction'),
-('Science Fiction');
+('Science Fiction'),
+('Fantasy'),
+('Children''s'),
+('Non-fiction'),
+('History'),
+('Cooking'),
+('Humour'),
+('Self-help');
+
+
+
 
 INSERT INTO genre_user (users_id, genres_id)
 VALUES (1, 1),
