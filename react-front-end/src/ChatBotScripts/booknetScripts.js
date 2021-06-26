7383a2c458b91b2b11e8d9550e76d10564227438
unknown <briannac@shaw.ca>
2021-06-26 01:04:05 +0100

    add a backend route for google books

--- react-front-end/src/ChatBotScripts/booknetScripts.js
+++ react-front-end/src/ChatBotScripts/booknetScripts.js
@@ -10,25 +10,6 @@ const heeeeeyReader = [
     message: "Heeeeey reader, ever had a novel encouter?",
     trigger: "2",
   },
-  {
-    id: 2,
-    options: [
-      {
-        value: "y",
-        label: "You're punny",
-        trigger: "yes-response",
-      },
-      { value: "n", label: "No", trigger: "no-response" },
-    ],
-  },
-  {
-    id: "yes-response",
-    message: "I'm good with words 🤓📚",
-  },
-  {
-    id: "no-response",
-    message: "Then you're in luck",
-  },
 ];
 
 const hi = [
