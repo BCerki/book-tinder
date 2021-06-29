443d594c4c297351790be7a2f9bd35462019af62
unknown <briannac@shaw.ca>
2021-06-29 22:11:04 +0100

    add a request for the google book description to the testing script

--- react-front-end/src/ChatBotScripts/testingScript.js
+++ react-front-end/src/ChatBotScripts/testingScript.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 
 import ChatbotSentPic from "../Components/ChatbotSentPic";
-import BooknetQuote from "../Components/BooknetQuote";
+import GoogleDescription from "../Components/GoogleDescription";
 import BooknetImagery from "../Components/BooknetImagery";
 
 const testingScript = [
@@ -24,14 +24,18 @@ const testingScript = [
     ),
     trigger: "4",
   },
-  { id: "4", message: "Click yes for a quote", trigger: "5" },
+  {
+    id: "4",
+    message: "Click yes for the book description from google",
+    trigger: "5",
+  },
   {
     id: "5",
     options: [{ value: 1, label: "yes", trigger: "6" }],
   },
   {
     id: "6",
-    component: <BooknetQuote />,
+    component: <GoogleDescription />,
     // message: "just kidding, not ready yet",
   },
 ];
