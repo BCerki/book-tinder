0ef9d8de3b5e14ac31fc4237d6cae3e8c7112250
unknown <briannac@shaw.ca>
2021-06-30 00:11:40 +0100

    update GoogleDescription component to use useMatches hook

--- react-front-end/src/Components/GoogleDescription.js
+++ react-front-end/src/Components/GoogleDescription.js
@@ -2,40 +2,34 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import secrets from "../.secrets";
+import useMatches from "../hooks/useMatches";
+import Loading from "./Loading";
 const { BOOKNET_TOKEN, GOOGLE_BOOK_KEY } = secrets;
 
 export default function GoogleDescription(props) {
-  const matchId = Number(useLocation().pathname.replace("/matches/", ""));
+  const { getChattingMatch } = useMatches();
+  const match = getChattingMatch();
   const [description, setDescription] = useState("");
 
-  const [match, setMatch] = useState();
-
   useEffect(() => {
-    if (matchId) {
+    if (match) {
       axios
-        .get(`/api/users/1/conversations`)
-        .then((result) => {
-          //set the match
-          const allMatches = result.data;
-          const thisMatch = allMatches.find((book) => book.id === matchId);
-          setMatch(thisMatch);
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=isbn:${match.isbn}&key=${GOOGLE_BOOK_KEY}`
+        )
+        .then((response) => {
+          const longDescription = response.data.items[0].volumeInfo.description;
 
-          axios
-            .get(
-              `https://www.googleapis.com/books/v1/volumes?q=isbn:${thisMatch.isbn}&key=${GOOGLE_BOOK_KEY}`
-            )
-            .then((response) => {
-              const longDescription =
-                response.data.items[0].volumeInfo.description;
-
-              setDescription(longDescription);
-            });
+          setDescription(longDescription);
         })
+
         .catch((err) => {
           console.log(err.message);
         });
     }
-  }, []);
-
+  }, [match]);
+  if (!match) {
+    return <Loading />;
+  }
   return <span className={"customText"}>{description}</span>;
 }
